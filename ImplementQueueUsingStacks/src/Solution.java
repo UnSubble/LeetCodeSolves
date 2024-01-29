0579bfcf5626c34895e603c8d8ed232cd5c8e465
İsmail Kulak <unsubble@gmail.com>
2024-01-29 16:22:52 +0300

    ImplementQueueUsingStacks solved.

---
+++ ImplementQueueUsingStacks/src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+class MyQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty())
+                stack2.push(stack1.pop());
+        }
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
