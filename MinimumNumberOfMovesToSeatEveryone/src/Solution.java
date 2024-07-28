9c8a2f4b3932149d4c783785bc2b9cae54daad5c
unsubble <unsubble@gmail.com>
2024-07-29 02:11:02 +0300

    MinimumNumberOfMovesToSeatEveryone solved.

---
+++ MinimumNumberOfMovesToSeatEveryone/src/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int res = 0;
+        for (int i = 0; i < seats.length; i++) {
+            res += Math.abs(seats[i] - students[i]);
+        }
+        return res;
+    }
+}
